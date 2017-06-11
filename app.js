d107c2061afe3088c6aa8e06f008f8954fd70c1b
susheelkbhargav <susheelkbhargav@gmail.com>
2017-06-11 00:18:30 -0700

    first commit

---
+++ app.js
@@ -0,0 +1,91 @@
+var express = require('express');
+var path = require('path');
+var favicon = require('static-favicon');
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+
+var routes = require('./routes/index');
+var users = require('./routes/users');
+
+var app = express();
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
+
+app.use(favicon());
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
+
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', routes);
+app.use('/users', users);
+
+/// catch 404 and forwarding to error handler
+app.use(function(req, res, next) {
+    var err = new Error('Not Found');
+    err.status = 404;
+    next(err);
+});
+
+/// error handlers
+
+// development error handler
+// will print stacktrace
+if (app.get('env') === 'development') {
+    app.use(function(err, req, res, next) {
+        res.status(err.status || 500);
+        res.render('error', {
+            message: err.message,
+            error: err
+        });
+    });
+}
+
+// production error handler
+// no stacktraces leaked to user
+app.use(function(err, req, res, next) {
+    res.status(err.status || 500);
+    res.render('error', {
+        message: err.message,
+        error: {}
+    });
+});
+
+
+module.exports = app;
+
+var twilio = require('twilio');
+var accountSid = 'AC97083564b3cb43a3279dcc48b0a976ed'; 
+var authToken = '16aec6db1fed7d66ba29b70d238d7535';   
+
+var twilio = require('twilio');
+var client = new twilio(accountSid, authToken);
+
+client.messages.create({
+    body: 'Hello from Sheacher a new channel for the topic has been created',
+    to: '+14083102536',  // phone
+    from: '+18312192418' // Twilio number
+})
+.then((message) => console.log(message.sid));
+
+
+
+app.post("/message", function (request, response) {
+  console.log(request.body.Body);
+  console.log(request.body.From);  
+//  response.send("<Response><Message>" + request.body.Body + "</Message></Response>");
+});
+
+app.get("/", function (request, response) {
+  response.sendFile(__dirname + '/views/index.html');
+});
+
+var listener = app.listen(process.env.PORT, function () {
+  console.log('Your app is listening on port ' + listener.address().port);
+});
+
